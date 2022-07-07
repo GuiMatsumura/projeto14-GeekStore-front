30ae3491c28a30e448c2a5be153d9a90bec7a886
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2022-07-07 17:48:49 -0300

    initial setup

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+
+import "../css/reset.css";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  );
+}
