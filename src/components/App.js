a5b057f90c372f577849481eeaf7a9133a68e5b3
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2022-07-08 01:40:29 -0300

    almost done, just need the api url

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 
 import "../css/reset.css";
+import Login from "./LoginScreen";
+import TokenContext from "./context/Token";
 
 export default function App() {
+  const [token, setToken] = React.useState("");
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route />
-      </Routes>
-    </BrowserRouter>
+    <TokenContext.Provider value={[token, setToken]}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/entrar" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </TokenContext.Provider>
   );
 }
